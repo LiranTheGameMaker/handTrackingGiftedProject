0d4f9443a9f892b91488dc9d339a43661e5db8ec
XythedMC <141483156+XythedMC@users.noreply.github.com>
2024-01-24 12:03:14 +0200

    added two draw modes: one using circles and on with lines between each point

--- handTrackingMain.py
+++ handTrackingMain.py
@@ -9,23 +9,33 @@ RT = "Right"
 LF = "Left"
 canvas = [int, int]
 
-#def maskOutput(image, canvas_size : list):
-   # return maskList.process_image(image, canvas_size)
+
+# def maskOutput(image, canvas_size : list):
+# return maskList.process_image(image, canvas_size)
 
 
 def main():
     bgImage = cv2.imread(r'img.png')
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(1)
     tracker = handTracker()
 
+    handPositionListRT = []
+    handPositionListLF = []
     handsList = HandsList()
     debugDraw = True  # Determine if to draw landmarks and label on screen
+    mode = False
 
     while True:
+        if cv2.waitKey(1) & 0xFF == ord('m'):
+            if not mode:
+                mode = True
+            else:
+                mode = False
+
         success, image = cap.read()
-        #canvas = [image.shape[1], image.shape[0]]
+        # canvas = [image.shape[1], image.shape[0]]
         image = cv2.flip(image, 1)
-        #bgImage = cv2.resize(bgImage, canvas)
+        # bgImage = cv2.resize(bgImage, canvas)
         image = tracker.processImage(image, debugDraw)
 
         handsList.clear()
@@ -39,20 +49,49 @@ def main():
                 detectedHand = SimpleHand(i, side, score, lmlist)
                 # Add this hand found to the list
                 handsList.addHand(detectedHand)
+
                 if detectedHand.side == RT:
                     if detectedHand.isIndexFingerUp():
-                        cv2.circle(bgImage, (detectedHand.getLandmarkX(HandLM.INDEX_FINGER_TIP), detectedHand.getLandmarkY(HandLM.INDEX_FINGER_TIP)), 10, (0, 0, 255), cv2.FILLED)
+                        if mode is True:
+                            cv2.circle(bgImage, (detectedHand.getLandmarkX(HandLM.INDEX_FINGER_TIP),
+                                                 detectedHand.getLandmarkY(HandLM.INDEX_FINGER_TIP)), 10, (0, 0, 255),
+                                       cv2.FILLED)
+                        else:
+                            if len(handPositionListRT) != 4:
+                                handPositionListRT.append(detectedHand.getLandmarkX(HandLM.INDEX_FINGER_TIP))
+                                handPositionListRT.append(detectedHand.getLandmarkY(HandLM.INDEX_FINGER_TIP))
+                            else:
+                                cv2.line(bgImage, (handPositionListRT[0], handPositionListRT[1]),
+                                         (handPositionListRT[2], handPositionListRT[3]), (0, 0, 255), 10, cv2.FILLED)
+                                handPositionListRT.remove(handPositionListRT[0])
+                                handPositionListRT.remove(handPositionListRT[0])
+
                 else:
                     if detectedHand.isIndexFingerUp():
-                        cv2.circle(bgImage, (detectedHand.getLandmarkX(HandLM.INDEX_FINGER_TIP), detectedHand.getLandmarkY(HandLM.INDEX_FINGER_TIP)), 10, (255, 0, 0), cv2.FILLED)
-
+                        if mode is True:
+                            cv2.circle(bgImage, (detectedHand.getLandmarkX(HandLM.INDEX_FINGER_TIP),
+                                                 detectedHand.getLandmarkY(HandLM.INDEX_FINGER_TIP)), 10, (255, 0, 0),
+                                       cv2.FILLED)
+                        else:
+                            if len(handPositionListLF) != 4:
+                                handPositionListLF.append(detectedHand.getLandmarkX(HandLM.INDEX_FINGER_TIP))
+                                handPositionListLF.append(detectedHand.getLandmarkY(HandLM.INDEX_FINGER_TIP))
+                            else:
+                                cv2.line(bgImage, (handPositionListLF[0], handPositionListLF[1]),
+                                         (handPositionListLF[2], handPositionListLF[3]), (255, 0, 0), 10, cv2.FILLED)
+                                handPositionListLF.remove(handPositionListLF[0])
+                                handPositionListLF.remove(handPositionListLF[0])
 
         print(handsList)
         if keyboard.is_pressed('b'):
             bgImage = cv2.imread(r"img.png")
         cv2.imshow("Video", image)
         cv2.imshow("Canvas", bgImage)
-        cv2.waitKey(1)
+        closeKey = cv2.waitKey(1) & 0xFF
+        print(closeKey)
+        if closeKey == ord('q'):
+            break
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
